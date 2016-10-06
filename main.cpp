d9341e9bd02020e63348226fd1fbf6452f6b61ca
tautret <autret.tristan@gmail.com>
2016-10-06 16:17:14 +0200

    Parser file

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include <map>
 #include "generator.cpp"
 #include "solver.cpp"
+#include "parser.cpp"
 
 using namespace std;
 int main (int argc, char *argv[]){
-    map<int,pair<int,int>> cards = generator(7,7,10);
+	parserFile(argv[1]);
+    map<int,pair<int,int>> cards = generator(200,200,20);
+    
     for (const auto &card : cards)
         cout << "Id Card : " << card.first << " / Color : " << card.second.first << " / Number : " << card.second.second << endl;
 
